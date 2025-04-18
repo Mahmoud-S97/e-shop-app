d207d559b11cd9b01755d1fc88929868f886b12d
Mahmoud-S97 <mhmodhr@gmail.com>
2025-04-18 23:33:59 +0100

    Added ContatUs-Screen and Google-Maps settings

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,22 @@
 export default CONSTANTS = {
-    LIST: 'LIST',
-    GRID: 'GRID',
-    SINGLE: 'SINGLE',
-    IS_LOGGED_IN: 'isLoggedIn',
-    ORDER_SUMMARY: 'Order Summary',
-    FAVORITE: 'Favorite',
-    LARGE: 'large',
-    SMALL: 'small'
-}
+  LIST: 'LIST',
+  GRID: 'GRID',
+  SINGLE: 'SINGLE',
+  IS_LOGGED_IN: 'isLoggedIn',
+  ORDER_SUMMARY: 'Order Summary',
+  FAVORITE: 'Favorite',
+  LARGE: 'large',
+  SMALL: 'small',
+  FACEBOOK: 'facebook',
+  INSTAGRAM: 'instagram',
+  LINKEDIN: 'linkedin'
+};
+
+export const SOCIAL_MEDIA_LINKS = {
+  FACEBOOK_APP: 'fb://profile/mahmoud.alsaleh.5876',
+  INSTAGRAM_APP: 'instagram://user?username=gold_store_jo',
+  LINKEDIN_APP: 'linkedin://in/mahmoud-al-saleh-a16303168',
+  FACEBOOK_WEB: 'https://www.facebook.com/mahmoud.alsaleh.5876',
+  INSTAGRAM_WEB: 'https://www.instagram.com/gold_store_jo',
+  LINKEDIN_WEB: 'https://www.linkedin.com/in/mahmoud-al-saleh-a16303168'
+};
