2fd55b0a413517550a0e7c12896187b0c5e8e5a2
Mahmoud-S97 <mhmodhr@gmail.com>
2025-07-02 17:20:25 +0100

    Replacing the App-Icon

--- src/constants/images/LocalImages.js
+++ src/constants/images/LocalImages.js
@@ -3,7 +3,7 @@ export const LOCAL_IMAGES = {
     STORE_LOGO: require('../../assets/images/shopping-bag.jpg'),
     EMPTY_CART: require('../../assets/icons/empty_cart.webp'),
     EMPTY_FAV_PRODUCT: require('../../assets/icons/empty_fav_product.webp'),
-    FEEDBACK_BG: require('../../assets/images/feedback_bg.png')
+    FEEDBACK_BG: require('../../assets/images/feedback_bg.jpg')
 }
 
 export const FAKE_IMAGES = {
