dc95d496d9b182519f49e39f68d503b3ee10c80b
Mahmoud Saleh <mahmoudsaleh@Mahmouds-MacBook-Pro.local>
2025-09-13 22:30:37 +0100

    Some layout enhancements on Feedback-Screen done

--- src/screens/Feedback/FeedbackScreen.js
+++ src/screens/Feedback/FeedbackScreen.js
@@ -1,30 +1,30 @@
-import React, {useCallback, useEffect} from 'react';
-import {View, Text, ImageBackground, ScrollView} from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import { View, Text, ImageBackground, ScrollView } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Carousel from 'react-native-reanimated-carousel';
 import FeedbackScreenStyles from './FeedbackScreenStyles';
-import {COLORS, GENERAL_STYLES} from '../../constants/styles/Styles';
-import {LOCAL_IMAGES} from '../../constants/images/LocalImages';
-import {getScreenWidth} from '../../utils';
-import {StarRatingDisplay} from 'react-native-star-rating-widget';
+import { COLORS, GENERAL_STYLES } from '../../constants/styles/Styles';
+import { LOCAL_IMAGES } from '../../constants/images/LocalImages';
+import { getScreenWidth } from '../../utils';
+import { StarRatingDisplay } from 'react-native-star-rating-widget';
 import MainButton from '../../components/Globals/MainButton';
 import SCREENS from '../../constants/screens';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchReviewersQuotes} from '../../api/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReviewersQuotes } from '../../api/User';
 import MainHeader from '../../components/Header/MainHeader';
 import MenuIcon from '../../components/Globals/MenuIcon';
 import FastImage from 'react-native-fast-image';
 import Spinner from '../../components/Globals/Spinner';
 
-const FeedbackScreen = ({navigation}) => {
+const FeedbackScreen = ({ navigation }) => {
   const dispatch = useDispatch();
-  const {reviewersQuotes} = useSelector(state => state.clientsSlice);
+  const { reviewersQuotes } = useSelector(state => state.clientsSlice);
 
   useEffect(() => {
     dispatch(fetchReviewersQuotes());
   }, [navigation, dispatch]);
 
-  const RenderCarouselItem = useCallback(({item}) => {
+  const RenderCarouselItem = useCallback(({ item }) => {
     return (
       <View style={FeedbackScreenStyles.carouselItemBox}>
         <View style={FeedbackScreenStyles.reviewerImgBox}>
@@ -42,7 +42,7 @@ const FeedbackScreen = ({navigation}) => {
           rating={item.rating}
           color={COLORS.PRIMARY}
           starSize={35}
-          style={{marginVertical: 10}}
+          style={{ marginVertical: 10 }}
         />
         <Text style={FeedbackScreenStyles.reviewerText} numberOfLines={8}>
           {item.quote}
@@ -55,10 +55,7 @@ const FeedbackScreen = ({navigation}) => {
   }, []);
 
   return (
-    <ScrollView
-      style={GENERAL_STYLES.scrollingView}
-      showsHorizontalScrollIndicator={false}
-      nestedScrollEnabled={true}>
+    <View style={GENERAL_STYLES.screen}>
       <MainHeader
         headerLeft={{
           headerLeftBtn1_content: <MenuIcon />,
@@ -71,45 +68,50 @@ const FeedbackScreen = ({navigation}) => {
         headerTitle="Feedback"
         headerRight={{}}
       />
-      <View style={GENERAL_STYLES.container}>
-        <ImageBackground
-          style={FeedbackScreenStyles.feedbackBG}
-          source={LOCAL_IMAGES.FEEDBACK_BG}>
-          <FontAwesome name="star" size={50} color={COLORS.WHITE} />
-          <Text style={FeedbackScreenStyles.feedbackText}>Feedback Form</Text>
-        </ImageBackground>
-        <View style={FeedbackScreenStyles.carouselBox}>
-          {reviewersQuotes.length > 0 ? (
-            <Carousel
-              data={reviewersQuotes}
-              loop
-              mode="parallax"
-              windowSize={3}
-              width={getScreenWidth() - 48}
-              height={450}
-              autoPlay={true}
-              autoPlayInterval={3000}
-              scrollAnimationDuration={1000}
-              renderItem={RenderCarouselItem}
-            />
-          ) : (
-            <Spinner />
-          )}
+      <ScrollView
+        style={GENERAL_STYLES.scrollingView}
+        showsHorizontalScrollIndicator={false}
+        nestedScrollEnabled={true}>
+        <View style={GENERAL_STYLES.container}>
+          <ImageBackground
+            style={FeedbackScreenStyles.feedbackBG}
+            source={LOCAL_IMAGES.FEEDBACK_BG}>
+            <FontAwesome name="star" size={50} color={COLORS.WHITE} />
+            <Text style={FeedbackScreenStyles.feedbackText}>Feedback Form</Text>
+          </ImageBackground>
+          <View style={FeedbackScreenStyles.carouselBox}>
+            {reviewersQuotes.length > 0 ? (
+              <Carousel
+                data={reviewersQuotes}
+                loop
+                mode="parallax"
+                windowSize={3}
+                width={getScreenWidth() - 48}
+                height={450}
+                autoPlay={true}
+                autoPlayInterval={3000}
+                scrollAnimationDuration={1000}
+                renderItem={RenderCarouselItem}
+              />
+            ) : (
+              <Spinner />
+            )}
+          </View>
+          <View style={FeedbackScreenStyles.footerBtnsBox}>
+            <MainButton
+              style={FeedbackScreenStyles.shoppingBtn}
+              onPress={() => navigation.navigate(SCREENS.HOME)}>
+              Shopping
+            </MainButton>
+            <MainButton
+              style={FeedbackScreenStyles.contactBtn}
+              onPress={() => navigation.navigate(SCREENS.CONTACT_US)}>
+              Contact Us
+            </MainButton>
+          </View>
         </View>
-        <View style={FeedbackScreenStyles.footerBtnsBox}>
-          <MainButton
-            style={FeedbackScreenStyles.shoppingBtn}
-            onPress={() => navigation.navigate(SCREENS.HOME)}>
-            Shopping
-          </MainButton>
-          <MainButton
-            style={FeedbackScreenStyles.contactBtn}
-            onPress={() => navigation.navigate(SCREENS.CONTACT_US)}>
-            Contact Us
-          </MainButton>
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 };
 
